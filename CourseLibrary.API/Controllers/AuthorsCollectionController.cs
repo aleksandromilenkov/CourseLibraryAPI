7e3051f0978325ddaeb0e96086fcfe8f299a5c01
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-01-18 20:50:51 +0100

    Creating Child Resources together with Parent Resource and Creating a Collection of Resources

---
+++ CourseLibrary.API/Controllers/AuthorsCollectionController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Entities;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsCollectionController : ControllerBase {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorsCollectionController(
+            ICourseLibraryRepository courseLibraryRepository,
+            IMapper mapper) {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<AuthorDto>>> CreateAuthorCollection(IEnumerable<AuthorForCreationDto> authorCollection) {
+            var authorEntities = _mapper.Map<IEnumerable<Author>>(authorCollection);
+            foreach (var author in authorEntities) {
+                _courseLibraryRepository.AddAuthor(author);
+            }
+            await _courseLibraryRepository.SaveAsync();
+            return Ok();
+        }
+
+    }
+}
