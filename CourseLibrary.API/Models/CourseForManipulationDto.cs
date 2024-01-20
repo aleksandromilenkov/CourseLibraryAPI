433af28f06db3df2329ad2b22f0e6b5082287b94
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-01-20 16:50:00 +0100

    Class Level Validation

--- CourseLibrary.API/Models/CourseForManipulationDto.cs
+++ CourseLibrary.API/Models/CourseForManipulationDto.cs
@@ -1,11 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models {
-    public class CourseForManipulationDto {
+    public class CourseForManipulationDto : IValidatableObject {
         [Required(ErrorMessage = "You should fill out a title.")]
         [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters")]
         public string Title { get; set; } = string.Empty;
         [MaxLength(1500, ErrorMessage = "The title shouldn't have more than 1500 characters")]
         public virtual string Description { get; set; } = string.Empty;
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) {
+            if (Title == Description) {
+                yield return new ValidationResult("The provided description should be different from the title.", new[] { "Course" });
+            }
+        }
     }
 }
---
+++ CourseLibrary.API/ValidationAttributes/CourseTitleMustBeDifferentToDescriptionAttribute.cs
@@ -0,0 +1,28 @@
+﻿using CourseLibrary.API.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.API.ValidationAttributes {
+    public class CourseTitleMustBeDifferentToDescriptionAttribute : ValidationAttribute {
+        public CourseTitleMustBeDifferentToDescriptionAttribute() {
+        }
+
+        protected override ValidationResult? IsValid(object? value,
+            ValidationContext validationContext) {
+            if (validationContext.ObjectInstance is not
+                CourseForManipulationDto course) {
+                throw new Exception($"Attribute " +
+                    $"{nameof(CourseTitleMustBeDifferentToDescriptionAttribute)} " +
+                    $"must be applied to a " +
+                    $"{nameof(CourseForManipulationDto)} or derived type.");
+            }
+
+            if (course.Title == course.Description) {
+                return new ValidationResult(
+                "The provided description should be different from the title.",
+                    new[] { nameof(CourseForManipulationDto) });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
